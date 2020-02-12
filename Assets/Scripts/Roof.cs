be9e5504b37f3c8e049e75cdb96529df40feebf7
Zambom <erikzambom@hotmail.com>
2020-02-12 06:48:01 -0300

    Adjusting duplicated destruction event

---
+++ Assets/Scripts/Roof.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roof : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "bullet")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
